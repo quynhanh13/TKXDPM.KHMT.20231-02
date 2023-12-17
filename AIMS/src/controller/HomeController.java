fd6c8089a43a2aee457d21e32b5d9a0a103485db
NguyenVuThucAnh <118005851+NguyenVuThucAnh@users.noreply.github.com>
2023-12-17 20:57:30 +0700

    solid nvta

--- AIMS/src/controller/HomeController.java
+++ AIMS/src/controller/HomeController.java
@@ -13,7 +13,16 @@ import entity.media.Media;
 //funtional cohesion
 public class HomeController extends BaseController{
 
-
+    /** Phương thức này lấy tất cả các đối tượng Media từ cơ sở dữ liệu và trả về chúng.
+     * VI PHẠM NGUYÊN TẮC SOLID:
+     * - VI PHẠM Single Responsibility Principle (Nguyên tắc đơn trách nhiệm):
+     *   + Lớp này không chỉ điều khiển luồng logic mà còn trực tiếp thực hiện truy vấn cơ sở dữ liệu để lấy dữ liệu Media.
+     *   + Điều này làm cho lớp này có nhiều trách nhiệm hơn cần thiết.
+     * 
+     * - VI PHẠM Dependency Inversion Principle (Nguyên tắc đảo ngược phụ thuộc):
+     *   + Lớp HomeController tạo trực tiếp một đối tượng Media bên trong phương thức để lấy dữ liệu từ cơ sở dữ liệu.
+     *   + Điều này tạo ra mối liên kết cứng giữa HomeController và lớp Media, không tuân theo nguyên tắc đảo ngược phụ thuộc.
+     * 
     /**
      * this method gets all Media in DB and return back to home to display
      * @return List[Media]
