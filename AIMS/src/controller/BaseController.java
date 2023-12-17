d41d783e8588f0a3fd22aef4208bf55d7a983b3f
NguyenVuThucAnh <118005851+NguyenVuThucAnh@users.noreply.github.com>
2023-12-17 21:01:49 +0700

    solid nvta

--- AIMS/src/controller/BaseController.java
+++ AIMS/src/controller/BaseController.java
@@ -12,7 +12,13 @@ import entity.media.Media;
  */
 //funtional cohesion
 public class BaseController {
-    
+    /**
+     * Phương thức này kiểm tra xem Media có trong giỏ hàng không, nếu có sẽ trả về CartMedia, ngược lại trả về null.
+     * VI PHẠM NGUYÊN TẮC SOLID:
+     * - VI PHẠM Single Responsibility Principle (Nguyên tắc đơn trách nhiệm):
+     *   + Lớp BaseController không chỉ là một controller cơ bản mà còn trực tiếp thực hiện logic liên quan đến Cart và Media.
+     *   + Nó không chỉ kiểm tra xem Media có trong Cart hay không mà còn trả về CartMedia, không giữ cho lớp này có trách nhiệm đơn giản.
+     * 
     /**
      * The method checks whether the Media in Cart, if it were in, we will return the CartMedia else return null
      * @param media
@@ -22,7 +28,13 @@ public class BaseController {
         // Content coupling
         return Cart.getCart().checkMediaInCart(media);
     }
-
+    /**
+     * Phương thức này lấy danh sách các mục trong giỏ hàng
+     * VI PHẠM NGUYÊN TẮC SOLID:
+     * - VI PHẠM Single Responsibility Principle (Nguyên tắc đơn trách nhiệm):
+     *   + Lớp BaseController không chỉ là một controller cơ bản mà còn trực tiếp thực hiện logic liên quan đến Cart và Media.
+     *   + Nó không chỉ lấy danh sách các mục trong Cart mà còn trả về danh sách các đối tượng CartMedia.
+     * 
     /**
      * This method gets the list of items in cart
      * @return List[CartMedia]
